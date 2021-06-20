7177c7a2a147f1e198613436762387cbd29cea62
JuanDelValle18 <jdelvalle585@gmail.com>
2021-06-20 15:01:14 -0600

    Update toinmenu.js

--- src/toinmenu.js
+++ src/toinmenu.js
@@ -1,20 +1,11 @@
 const toinmenu = (prefix, pushname) => {
-    return `◪ *Comandos de Juan*
+    return `◪ *Comandos de Destroyer*
     │
     ├─ ❏ ${prefix}setprefix
     ├─ ❏ ${prefix}block
     ├─ ❏ ${prefix}bc
     ├─ ❏ ${prefix}bcgc
     └─ ❏ ${prefix}clearall`
-    ├─ ❏ ${prefix}addbadword
-    ├─ ❏ ${prefix}delbadword
-    ├─ ❏ ${prefix}kickall
-    ├─ ❏ ${prefix}setreply
-    ├─ ❏ ${prefix}unblock
-    ├─ ❏ ${prefix}leave
-    ├─ ❏ ${prefix}event [1/0]
-    ├─ ❏ ${prefix}clone
-    └─ ❏ ${prefix}setppbot
 
 }
 
