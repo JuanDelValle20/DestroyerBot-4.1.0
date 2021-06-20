16909462b8aa3e1dbdb5154fa9ebaeed05071938
JuanDelValle18 <jdelvalle585@gmail.com>
2021-06-20 14:02:35 -0600

    Create toinmenu.js

---
+++ src/toinmenu.js
@@ -0,0 +1,21 @@
+const toinmenu = (prefix, pushname) => {
+    return `◪ *Comandos de Juan*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+    ├─ ❏ ${prefix}addbadword
+    ├─ ❏ ${prefix}delbadword
+    ├─ ❏ ${prefix}kickall
+    ├─ ❏ ${prefix}setreply
+    ├─ ❏ ${prefix}unblock
+    ├─ ❏ ${prefix}leave
+    ├─ ❏ ${prefix}event [1/0]
+    ├─ ❏ ${prefix}clone
+    └─ ❏ ${prefix}setppbot
+
+}
+
+exports.toinmenu = toinmenu
